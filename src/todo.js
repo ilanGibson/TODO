c383976c2643ea637b6cb86984d6785c54eeac08
ilanGibson <ilanjgibson@gmail.com>
2023-12-30 18:44:23 -0700

    ADD src/ index.js project.js todo.js style.css

---
+++ src/todo.js
@@ -0,0 +1,44 @@
+class Todo {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.completed = false;
+        
+    }
+    toggleCompleted() {
+        this.completed = !this.completed;
+    }
+    setTitle(newTitle) {
+        this.title = newTitle;
+    }
+
+    getTitle() {
+        return this.title;
+    }
+
+    setDescription(newDescription) {
+        this.description = newDescription;
+    }
+
+    getDescription() {
+        return this.description;
+    }
+
+    setDueDate(newDueDate) {
+        this.dueDate = newDueDate;
+    }
+
+    getDueDate() {
+        return this.dueDate;
+    }
+    
+    setPriority(newPriority) {
+        this.priority = newPriority;
+    }
+
+    getPriority() {
+        return this.priority;
+    }
+}
