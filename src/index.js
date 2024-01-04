840d037db3b550f770a1043bc092abc3c63b964f
ilanGibson <ilanjgibson@gmail.com>
2024-01-04 10:44:58 -0700

    ADD project list to hold projects

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./style.css";
+import { Todo } from "./todo.js";
+import { Project } from "./project.js";
+
+const ProjectList = (() => {
+  const projects = [];
+  const addProject = (project) => {
+    projects.push(project);
+  };
+
+  const getProjects = () => {
+    return projects;
+  };
+
+  return { addProject, getProjects };
+})();
+
+ProjectList.addProject(new Project());
+console.log(ProjectList.getProjects());
+
