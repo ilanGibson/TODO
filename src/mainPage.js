296378f70d33057b3ae158bb3621d65d82dc9025
ilanGibson <ilanjgibson@gmail.com>
2024-01-07 16:53:53 -0700

    FIX capitzlization of function name ADD new function to load projects to sidebar

--- src/mainPage.js
+++ src/mainPage.js
@@ -1,4 +1,6 @@
-export default function MainPage() {
+import { getProjectNames } from "./loadProject.js";
+
+export default function mainPage() {
     const wrap = document.createElement("div");
     wrap.classList.add("wrap");
 
@@ -11,9 +13,26 @@ export default function MainPage() {
     sidebarTitle.classList.add("sidebar-title");
     sidebarTitle.textContent = "Projects";
     sidebar.appendChild(sidebarTitle);
-    
+    loadProjects(sidebar);
 
     wrap.appendChild(sidebar);
     wrap.appendChild(main);
     document.body.appendChild(wrap);
+}
+
+function loadProjects(sidebar) {
+    const projectNames = getProjectNames();
+    console.log(projectNames);
+    projectNames.forEach(projectName => {
+        console.log(projectName);
+        // const project = loadProject(projectName);
+        const projectDiv = document.createElement("div");
+        projectDiv.classList.add("sidebar-project");
+        projectDiv.classList.add(projectName.replace(/\s/g, ""));
+        projectDiv.textContent = projectName;
+        projectDiv.addEventListener("click", () => {
+            // loadProjectPage(project);
+        });
+        sidebar.appendChild(projectDiv);
+    });
 }
