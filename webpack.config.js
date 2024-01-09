1bf9a9833814b1e9d308ca35ef3550c4b53e0dde
ilanGibson <ilanjgibson@gmail.com>
2024-01-08 18:27:32 -0700

    FIX image loader file configuration

--- webpack.config.js
+++ webpack.config.js
@@ -22,8 +22,17 @@ module.exports = {
         },
         {
             test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  name: '[path][name].[ext]',
+                  outputPath: 'images/',
+                  publicPath: 'images/',
+                }
+              },
+            ],
         }
-        ]
+      ] // Add closing parenthesis here
     }
-};
+}; // Add closing parenthesis here
